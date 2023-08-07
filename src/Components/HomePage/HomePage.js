dec7962ac22f2757d67035b54acbd24ecac6eb6e
Ol1mowski <olimarki123@wp.pl>
2023-08-07 11:27:28 +0200

    add starting react file

---
+++ src/Components/HomePage/HomePage.js
@@ -0,0 +1,17 @@
+import style from './HomePage.module.scss';
+
+const HomePage = () => {
+    return (
+        <>
+            <div className={style.homeContainer}>
+                <div className={style.homeContainer__left}>
+                    
+                </div>
+                <div className={style.homeContainer__right}>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default HomePage;
