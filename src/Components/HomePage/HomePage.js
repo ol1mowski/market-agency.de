b4dc746b61321d258885690e15ac5683554ff922
Ol1mowski <olimarki123@wp.pl>
2023-08-07 11:57:26 +0200

    add first section

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,11 +1,23 @@
 import style from './HomePage.module.scss';
+import logo from '../../assets/icons/logo.svg';
 
 const HomePage = () => {
     return (
         <>
             <div className={style.homeContainer}>
                 <div className={style.homeContainer__left}>
-                    
+                    <img src={logo} alt='logo' className={style.homeContainer__left__logo} />
+                    <div className={style.homeContainer__left__content}>
+                        <h2>
+                            Jeden krok do optymalizacji Twojej firmy
+                        </h2>
+                        <p>
+                            Agencja Market+
+                        </p>
+                        <button>
+                            Dowiedz się więcej
+                        </button>
+                    </div>
                 </div>
                 <div className={style.homeContainer__right}>
                 </div>
